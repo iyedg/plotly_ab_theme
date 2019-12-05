9dcaf530f068298d4114c880b99bd4d2ea06af58
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-12-05 18:44:16 +0100

    Initial version of theme

---
+++ plotly_ab_theme/ab_theme.py
@@ -0,0 +1,37 @@
+import plotly.io as pio
+import plotly.graph_objects as go
+
+W = 1200
+H = W / (16 / 9)
+layout = {
+    "layout": {
+        "annotationdefaults": {
+            "arrowcolor": "#2a3f5f",
+            "arrowhead": 0,
+            "arrowwidth": 1,
+        },
+        "coloraxis": {"colorbar": {"outlinewidth": 0, "ticks": ""}},
+        "font": {"family": "JF Flat"},
+        "hoverlabel": {"align": "left"},
+        "hovermode": "closest",
+        "paper_bgcolor": "white",
+        "shapedefaults": {"line": {"color": "#2a3f5f"}},
+        "title": {"x": 1, "font": dict(size=26), "xref": "paper"},
+        "xaxis": {"automargin": True, "ticks": "", "title": {"standoff": 15}},
+        "yaxis": {
+            "automargin": True,
+            "ticks": "",
+            "title": {"standoff": 15},
+            "side": "right",
+            "showline": False,
+        },
+        "width": W,
+        "height": H,
+        "legend": {"orientation": "h"},
+    }
+}
+pio.templates["ab_theme_ar"] = go.layout.Template(layout)
+
+layout["layout"]["font"]["family"] = "Roboto Condensed"
+layout["layout"]["yaxis"]["side"] = "left"
+pio.templates["ab_theme_fr"] = go.layout.Template(layout)
